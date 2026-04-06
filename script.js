c9fc5eb81eabc4f92a216c200e1b22da02b73c7c
Hugo Chimendes <hugo.leite1201@gmail.com>
2026-04-06 16:42:02 -0300

    feat: Adicionando funcionalidade no jogo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const canvasEl = document.querySelector("canvas");
-const canvasCtx = document.canvasEl("2d");
+const canvasCtx = canvasEl.getContext("2d");
 
 // Configurações do Jogo
 const tamBloco = 30;
@@ -105,9 +105,10 @@ const jogo = {
   // Criação de nova peça:
   novaPeca() {
     const id = this.proxima;
+    
     this.peca = {
       id,
-      forma: peca[id],
+      forma: pecas[id],
       x: rand(cols - pecas[id][0].length),
       y: 0,
     };
@@ -137,7 +138,7 @@ const jogo = {
         // this.peca.y + i = posição vertical da célula da matriz
         // dx/dy = deslocamento que estamos testando
         const x = this.peca.x + j + dx;
-        const y = this.peca.x + i + dy;
+        const y = this.peca.y + i + dy;
 
         // Limite das paredes e chão
         if (x < 0 || x >= cols || y >= rows) return false;
@@ -185,24 +186,161 @@ const jogo = {
     //
     //
     forma.forEach((linha, i) => {
-        linha.forEach((v, j) => {
-            if (v){
-                //
-                this.grid[y +i][x + j] = id;
-            }
-        });
+      linha.forEach((v, j) => {
+        if (v) {
+          //
+          this.grid[y + i][x + j] = id;
+        }
+      });
     });
 
     //
-    const linhas = this.limpar()
-    
-    //
-    //
+    const linhas = this.limpar();
+
+    // Pontuação:
+    // Cada linha removida vale 100 pontos
     this.pontos += linhas * 100;
 
-    //
+    // Cria a prox peça
     this.novaPeca();
+  },
+
+  limpar() {
+    let removidas = 0;
+
+    this.grid = this.grid.filter((linha) => {
+      // every(c => c !== -1) significa que todas as células forem diferentes de -1
+      // Então, signigica linha completa
+      if (linha.every((c) => c !== -1)) {
+        removidas++;
+        return false; // Remove a linha do array
+      }
+      return true; // Mantém a linha
+    });
+
+    // Depois de remover linhas, precisamos manter o grid
+    // Então adicionamos linhas novas no topo
+    while (this.grid.length < rows) {
+      this.grid.unshift(Array(cols).fill(-1));
+    }
+    return removidas;
   },
 };
 
+// Desenha na tela:
+// Desenha um bloco individual no canvas
+function bloco(x, y, cor) {
+  canvasCtx.fillStyle = cor;
+
+  // O "+1" cria um pequeno espaço entre os bloco
+  // "tamBloco - 2" pq o bloco fica 1px menor de cada lado
+  canvasCtx.fillRect(x + 1, y + 1, tamBloco - 2, tamBloco - 2);
+}
+
+function desenhar() {
+  if (!jogo.peca) return;
+
+  // Fundo do jogo
+  canvasCtx.fillStyle = "#222";
+  canvasCtx.fillRect(0, 0, canvasEl.width, canvasEl.height);
+
+  // Desenha o grid:
+  jogo.grid.forEach((linha, i) => {
+    linha.forEach((v, j) => {
+      bloco(j * tamBloco, i * tamBloco, v === -1 ? "#333" : cores[v]);
+    });
+  });
+
+  // Desenha a peça atual:
+  jogo.peca.forma.forEach((linha, i) => {
+    linha.forEach((v, j) => {
+      if (v) {
+        bloco(
+          (jogo.peca.x + j) * tamBloco,
+          (jogo.peca.y + i) * tamBloco,
+          cores[jogo.peca.id],
+        );
+      }
+    });
+  });
+
+  // Desenhar prox peça:
+  const p = pecas[jogo.proxima];
+  p.forEach((linha, i) => {
+    linha.forEach((v, j) => {
+      if (v) {
+        bloco(
+          cols * tamBloco + 20 + j * tamBloco,
+          60 + i * tamBloco,
+          cores[jogo.proxima],
+        );
+      }
+    });
+  });
+
+  // Texto da interface:
+  canvasCtx.fillStyle = "#fff";
+  canvasCtx.font = "16px Arial";
+  canvasCtx.textAlign = "left";
+
+  // Mostra a pontuação
+  canvasCtx.fillText("Pontos: " + jogo.pontos, cols * tamBloco + 10, 30);
+
+  // Título
+  canvasCtx.fillText("Próxima peça: ", cols * tamBloco + 10, 50);
+
+  // Tela Gamer Over:
+  if (jogo.gameOver) {
+    canvasCtx.fillStyle = "#000";
+    canvasCtx.fillRect(0, 0, canvasEl.width, canvasEl.height);
+
+    canvasCtx.fillStyle = "red";
+    canvasCtx.textAlign = "center";
+    canvasCtx.font = "30px Arial";
+    canvasCtx.fillText("GAME OVER", canvasEl.width / 2, canvasEl.height / 2);
+
+    canvasCtx.font = "16px Arial";
+    canvasCtx.fillText(
+      "Pressione 'R' para reiniciar",
+      canvasEl.width / 2,
+      canvasEl.width / 2 + 30,
+    );
+  }
+}
+
+// Controles do jogo:
+document.addEventListener("keydown", (e) => {
+  // Reiniciar o jogo
+  if (e.key === "r") {
+    jogo.iniciar(0);
+    return;
+  }
+  // Mover para esquerda
+  if (e.key === "ArrowLeft") jogo.mover(-1, 0);
+  // Mover para direita
+  if (e.key === "ArrowRight") jogo.mover(1, 0);
+  // Mover para baixo
+  if (e.key === "ArrowDown") jogo.mover(0, 1);
+  // Rotacionar
+  if (e.key === "ArrowUp") jogo.girar();
+});
+
+// Loop principal do jogo:
+let tempo = 0;
+function loop() {
+  tempo++;
+
+  if (tempo % 30 === 0) {
+    jogo.mover(0, 1);
+  }
+
+  // Desenha tudo na tela
+  desenhar();
+
+  // Pede para o navegador chamar esta função de novo no próximo frame
+  requestAnimationFrame(loop);
+}
+
+// Inicio do jogo:
 jogo.iniciar();
+loop();
